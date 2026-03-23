b9d94c03c0d96487003a44ae7a379cbc38ef7537
Sajeena <sajisaji17122004@gmail.com>
2026-03-23 16:40:10 +0530

    push

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,10 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   build: {
-    outDir: 'dist',
+    outDir: "dist",
   },
   plugins: [react()],
-  base: "/DTMS/",
+  base: process.env.VERCEL ? "/" : "/DTMS/",
   server: {
     port: 5173,
     proxy: {
